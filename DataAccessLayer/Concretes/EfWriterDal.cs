14199f01dda52e964aec4c5024df5598ceabc76f
Cengizhan KORKMAZ <cengizhankorkmazz@yahoo.com>
2022-08-30 12:48:50 +0300

    entity framework configuration added

---
+++ DataAccessLayer/Concretes/EfWriterDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Abstracts;
+using DataAccessLayer.Contexts;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concretes;
+
+namespace DataAccessLayer.Concretes
+{
+    public class EfWriterDal:EfRepositoryBase<Writer,Context>,IWriterDal
+    {
+    }
+}
---
+++ DataAccessLayer/Repositories/EfRepositoryBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Abstracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Repositories
+{
+    public class EfRepositoryBase<T, TContext> : IRepositoryDal<T> where T : class, IEntity, new() where TContext : DbContext, new()
+    {
+        public List<T> GetAll(Expression<Func<T, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null ? context.Set<T>().ToList() : context.Set<T>().Where(filter).ToList();
+            }
+        }
+
+        public T Get(Expression<Func<T, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<T>().SingleOrDefault(filter);
+
+            }
+        }
+
+        public void Add(T entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(T entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(T entity)
+        {
+             using (TContext context = new TContext())
+             {
+                 var deletedEntity = context.Entry(entity);
+                 deletedEntity.State = EntityState.Deleted;
+                 context.SaveChanges();
+             }
+        }
+    }
+}
